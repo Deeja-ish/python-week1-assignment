35929fc07f4a2f6dd11dd277f9a461d64cf16e2f
Khadija ismail Goni <khadijaismail696@gmail.com>
2025-07-30 21:53:35 +0100

    completed

---
+++ calculator.py
@@ -0,0 +1,20 @@
+# prompting the user to enter two digits
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+# Addition
+addition = num1 + num2
+print(f"The sum is: {addition}")
+
+# Subtraction
+subtraction = num1 - num2
+print(f"The difference is: {subtraction}")
+
+# Division
+division = num1 / num2
+print(f"The quotient is: {division}")
+
+# Multiplication
+multiplication = num1 * num2
+print(f"The product is: {multiplication}")
